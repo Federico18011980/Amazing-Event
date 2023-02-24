27568290612501c61e58c069683644a097184494
Federico18011980 <101432540+Federico18011980@users.noreply.github.com>
2023-02-23 22:18:03 -0300

    creando archivos js

---
+++ assets/js/home.js
@@ -0,0 +1,4 @@
+let fila = document.getElementById("fila-cards");//obtengo el contenedor fila 
+let contCard = document.getElementById("contenedor-card"); // devuelve el primer contenedor
+let card = document.getElementsByClassName("card"); //devuelve un array con las cards
+console.log(card);
